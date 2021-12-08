217c540bb7f7b538979b318c74223f7fd2a0ba85
Marcin Junikiewicz <mjunikiewicz@gmail.com>
2021-12-08 19:01:47 +0100

    add example unit tests (#4)
    Co-authored-by: Marcin Junikiewicz <Marcin.Junikiewicz@onexgroup.pl>

---
+++ src/api/Tests/Budgethold.UnitTests/App/Extensions/ControllerBaseExtensionsTests.cs
@@ -0,0 +1,100 @@
+﻿using Budgethold.API.Extensions;
+using Budgethold.Domain.Common;
+using Budgethold.Domain.Common.Errors;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using Xunit;
+using static Budgethold.UnitTests.App.Extensions.ControllerBaseExtensionsTests;
+
+namespace Budgethold.UnitTests.App.Extensions
+{
+    public class ControllerBaseExtensionsTests
+    {
+        #region GetResponseFromResult
+        [Fact]
+        public void GetResponseFromResult_EmptyResult_NoContent()
+        {
+            // Arrange
+            var controllerBase = new TestController();
+            var result = new Result();
+
+            // Act
+            var response = controllerBase.GetResponseFromResult(result);
+
+            // Assert
+            Assert.IsType<NoContentResult>(response);
+        }
+
+        [Fact]
+        public void GetResponseFromResult_NotEmptySuccesResult_Ok()
+        {
+            // Arrange
+            var controllerBase = new TestController();
+            var testDto = new TestDto { Id = 1, Name = "Stefan" };
+            var result = new Result<TestDto>(testDto);
+
+            // Act
+            var response = controllerBase.GetResponseFromResult(result);
+
+            // Assert
+            Assert.IsType<OkObjectResult>(response);
+            var responseValue = ((OkObjectResult)response).Value;
+            Assert.NotNull(responseValue);
+            Assert.IsType<TestDto>(responseValue);
+            var returnedDto = (TestDto)responseValue!;
+            Assert.Equal(testDto.Id, returnedDto.Id);
+            Assert.Equal(testDto.Name, returnedDto.Name);
+        }
+
+        [Fact]
+        public void GetResponseFromResult_NotFoundError_BadRequest()
+        {
+            // Arrange
+            var controllerBase = new TestController();
+            var errorMessage = "Nie znaleziono";
+            var error = new NotFoundError(errorMessage);
+            var result = new Result(error);
+
+            // Act
+            var response = controllerBase.GetResponseFromResult(result);
+
+            // Assert
+            Assert.IsType<BadRequestObjectResult>(response);
+            var responseValue = ((BadRequestObjectResult)response).Value;
+            Assert.Equal(errorMessage, responseValue);
+        }
+
+        [Fact]
+        public void GetResponseFromResult_UnhandledError_UnhandledErrorResultException()
+        {
+            // Arrange
+            var controllerBase = new TestController();
+            var error = new TestError("new type of error");
+            var result = new Result(error);
+
+            // Act => Assert
+            Assert.Throws<NotSupportedException>(() => controllerBase.GetResponseFromResult(result));
+        }
+        #endregion
+
+        #region Private helpers
+        public class TestController : ControllerBase { }
+
+        public record TestError : Error
+        {
+            public TestError(string original) : base(original) { }
+        }
+
+        public record TestDto
+        {
+            public TestDto()
+            {
+                Name = null!;
+            }
+
+            public int Id { get; init; }
+            public string Name { get; init; }
+        }
+        #endregion
+    }
+}
---
+++ src/api/Tests/Budgethold.UnitTests/Core/Application/Commands/Transaction/EditTransactionCommandHandlerTests.cs
@@ -0,0 +1,60 @@
+﻿using Budgethold.Application.Commands.Transaction.EditTransaction;
+using Budgethold.Application.Contracts.Persistance;
+using Budgethold.Domain.Common.Errors;
+using Moq;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Budgethold.UnitTests.Core.Application.Commands.Transaction
+{
+    public class EditTransactionCommandHandlerTests
+    {
+        [Fact]
+        public async Task Handle_NotExistingTransaction_NotFoundErrorNoChangesToDatabase()
+        {
+            // Arrange
+            var mockedUnitOfWork = new Mock<IUnitOfWork>();
+
+            mockedUnitOfWork.Setup(x => x.TransactionRepository.GetTransactionAsync(It.IsAny<int>(), It.IsAny<CancellationToken>()))
+               .ReturnsAsync(null as Domain.Models.Transaction);
+            mockedUnitOfWork.Setup(x => x.UserWalletsRepository.CheckIfUserIsAssignedToWalletAsync(It.IsAny<int>(), It.IsAny<int>(), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(false);
+
+            var commandHandler = new EditTransactionCommandHandler(mockedUnitOfWork.Object);
+            var command = new EditTransactionCommand(1, 1, "name", "desc", 1, 1M, DateTime.Parse("2011-03-21 13:26"), 1);
+
+            // Act
+            var result = await commandHandler.Handle(command, CancellationToken.None);
+
+            // Assert
+            Assert.NotNull(result.Error);
+            Assert.IsType<NotFoundError>(result.Error);
+            mockedUnitOfWork.Verify(x => x.SaveChangesAsync(It.IsAny<CancellationToken>()), Times.Never);
+        }
+
+        [Fact]
+        public async Task Handle_ExistingTransactionUserNotAssignedToWallet_NotFoundErrorNoChangesToDatabase()
+        {
+            // Arrange
+            var mockedUnitOfWork = new Mock<IUnitOfWork>();
+
+            mockedUnitOfWork.Setup(x => x.TransactionRepository.GetTransactionAsync(It.IsAny<int>(), It.IsAny<CancellationToken>()))
+               .ReturnsAsync(new Domain.Models.Transaction());
+            mockedUnitOfWork.Setup(x => x.UserWalletsRepository.CheckIfUserIsAssignedToWalletAsync(It.IsAny<int>(), It.IsAny<int>(), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(false);
+
+            var commandHandler = new EditTransactionCommandHandler(mockedUnitOfWork.Object);
+            var command = new EditTransactionCommand(1, 1, "name", "desc", 1, 1M, DateTime.Parse("2011-03-21 13:26"), 1);
+
+            // Act
+            var result = await commandHandler.Handle(command, CancellationToken.None);
+
+            // Assert
+            Assert.NotNull(result.Error);
+            Assert.IsType<NotFoundError>(result.Error);
+            mockedUnitOfWork.Verify(x => x.SaveChangesAsync(It.IsAny<CancellationToken>()), Times.Never);
+        }
+    }
+}
